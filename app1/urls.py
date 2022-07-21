05e5f3d3756992dd1f1ef636421bcac0ee2a7430
Ezequiel Camino <ezequiel.camino.96@gmail.com>
2022-07-21 19:32:26 -0300

    se agrega mostrar,eliminar y editar a la lista

--- app1/urls.py
+++ app1/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import crear_persona, vista, listado_persona
+from . import views
 #poner el INDEX HTML EN EL PATH VACIO
 
 urlpatterns = [
     path('', vista, name='index'),
     path('crear-persona/', crear_persona, name='crear_persona'),
     path('persona/', listado_persona, name='listado_personas'),
+    path('editar-persona/<int:pk>', views.EditarPersona.as_view(), name='editar_persona'),
+    path('eliminar-persona/<int:pk>', views.EliminarPersona.as_view(), name='eliminar_persona'),
+    path('mostrar-persona/<int:pk>', views.MostrarPersona.as_view(), name='mostrar_persona')
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import redirect, render
+from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic import DetailView
 
 from .forms import BusquedaPersona, FormPersona
 from .models import Persona
 from datetime import datetime
 
+
 # Create your views here.
 def vista(request):
     return render(request, 'index.html')
+
 
 def crear_persona(request):
     
@@ -46,3 +50,22 @@ def listado_persona(request):
     
     form= BusquedaPersona()
     return render(request, 'listado_personas.html', {'listado_personas': listado_personas,'form': form})
+
+
+class EditarPersona(UpdateView):
+    model: Persona
+    template_name = 'editar_persona.html'
+    success_url= "listado_personas"
+    fields= ['nombre', 'edad', 'fecha_creacion']
+
+
+class EliminarPersona(DeleteView):
+    model: Persona
+    template_name = 'eliminar_persona.html'
+    success_url= "listado_personas"
+
+
+class MostrarPersona(DetailView):
+    model: Persona
+    template_name = 'mostrar_persona.html'
+    success_url= "listado_personas"
